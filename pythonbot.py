79214679e21affefe9d28a064dcdd3c547b6f609
esteban own <crls.esteban@hotmail.com>
2020-02-10 23:46:34 -0500

    added the code to login, like and dislike

--- pythonbot.py
+++ pythonbot.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
-import secrets
+from secrets import username, password
 
+import time
 # driver_path = '/chromedriver_win32/chromedriver'
 # driver = webdriver.Chrome(executable_path=driver_path)
 
@@ -8,7 +9,6 @@ import secrets
 # options.binary_location = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
 # driver = webdriver.Chrome(chrome_options=options, executable_path="C:/Utility/BrowserDrivers/chromedriver.exe")
 
-print(secrets.usuario)
 class Bot():
     def __init__(self):
         # options = webdriver.ChromeOptions()
@@ -16,14 +16,44 @@ class Bot():
         driver = webdriver.Chrome("./chromedriver_win32/chromedriver.exe")
         self.driver = driver
 
-        self.driver.get("https://facebook.com")
+    def login(self):
+        self.driver.get("https://tinder.com")
+        time.sleep(4)
 
-        log_btn = self.driver.find_element_by_xpath('//*[@id="loginbutton"]')
+        fb_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
+        fb_btn.click()
 
-        user_in = self.driver.find_element_by_xpath('//*[@id="email"]')
-        pass_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        # switch to login popup
+        base_window = self.driver.window_handles[0]
+        self.driver.switch_to_window(self.driver.window_handles[1])
 
-        user_in.send_keys(secrets.usuario)
-        pass_in.send_keys(secrets.password)
-        
-        log_btn.click()
+        email_in = bot.driver.find_element_by_xpath('//*[@id="email"]')
+        pass_in = bot.driver.find_element_by_xpath('//*[@id="pass"]')
+
+        email_in.send_keys(username)
+        pass_in.send_keys(password)
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
+        login_btn.click()
+
+        # go back to the base window
+        self.driver.switch_to_window(base_window)
+
+        time.sleep(2)
+        # pass popups
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]/span')
+        popup_1.click()                                
+        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]/span')
+        popup_2.click()
+
+        popup_1 = bot.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]/span')
+
+    def like(self):
+        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
+        like_btn.click()
+
+    def dislike(self):
+        dislike_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[1]')
+        dislike_btn.click()
+bot = Bot()
+bot.login()
